058f841c63dbf8ea21465bb77b1b31ff07899e96
Oleksii Ulianov <alexallorayz@gmail.com>
2024-05-25 15:37:03 +0300

    Split Comment component

---
+++ lesson3-components-props/task3-split-comment-component/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Comment from './Comment';
+
+const userInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://avatars1.githubusercontent.com/u/30047064?v=4',
+};
+
+const App = () => {
+  return (
+    <Comment
+      user={userInfo}
+      text="Good job!"
+      date={new Date('2019-01-01T11:32:19.566z')}
+    />
+  );
+};
+
+export default App;
