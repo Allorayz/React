8ec756cab895fa54952344319fe6875f17706906
Oleksii Ulianov <alexallorayz@gmail.com>
2024-05-26 00:37:17 +0300

    Good button target

---
+++ lesson5-events/task2-good-button-target/src/GoodButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class GoodButton extends React.Component {
+  render() {
+    return (
+      <button
+        className="fancy-button"
+        onClick={event => alert(event.target.textContent)}
+      >
+        Click me!
+      </button>
+    );
+  }
+}
+
+export default GoodButton;
