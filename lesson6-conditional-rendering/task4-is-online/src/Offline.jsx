abc584b5be312cf5bbcdaf35c5f463fdbd451c78
Oleksii Ulianov <alexallorayz@gmail.com>
2024-05-26 23:28:19 +0300

    Is online

--- lesson6-conditional-rendering/task4-is-online/src/Offline.jsx
+++ lesson6-conditional-rendering/task4-is-online/src/Offline.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Offline = () => (
   <>
     <span className={'status__text'}>Offline</span>
+    <button className="status__btn">Reconnect</button>
   </>
 );
 
--- lesson6-conditional-rendering/task4-is-online/src/Status.jsx
+++ lesson6-conditional-rendering/task4-is-online/src/Status.jsx
@@ -2,29 +2,8 @@ import React from 'react';
 import Online from './Online';
 import Offline from './Offline';
 
-class Status extends React.Component {
-  state = {
-    isOnline: false,
-  };
-
-  setStatus = () => {
-    let newStatus = this.state.isOnline === false ? true : false;
-    console.log(newStatus);
-    this.setState({
-      isOnline: newStatus,
-    });
-  };
-
-  render() {
-    return (
-      <div className={'status'}>
-        {!this.state.isOnline ? <Offline /> : <Online />}
-        <button className="status__btn" onClick={() => this.setStatus()}>
-          Reconnect
-        </button>
-      </div>
-    );
-  }
-}
+const Status = ({ isOnline }) => (
+  <div className="status">{!isOnline ? <Offline /> : <Online />}</div>
+);
 
 export default Status;
