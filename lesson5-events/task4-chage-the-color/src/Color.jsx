d0e8caada6457f24b612f609a55dfd960393b270
Oleksii Ulianov <alexallorayz@gmail.com>
2024-05-26 12:42:18 +0300

    Change the color

---
+++ lesson5-events/task4-chage-the-color/src/Color.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const colors = {
+  RED: '#f00',
+  GREEN: '#0f0',
+  BlUE: '#00f',
+};
+
+class Color extends React.Component {
+  setBGcolor = color => {
+    document.body.style.backgroundColor = color;
+  };
+
+  render() {
+    return (
+      <div className={'colors'}>
+        <button
+          className={'colors__button'}
+          style={{ backgroundColor: colors.RED }}
+          onClick={() => this.setBGcolor(colors.RED)}
+        ></button>
+        <button
+          className={'colors__button'}
+          style={{ backgroundColor: colors.GREEN }}
+          onClick={() => this.setBGcolor(colors.GREEN)}
+        ></button>
+        <button
+          className={'colors__button'}
+          style={{ backgroundColor: colors.BlUE }}
+          onClick={() => this.setBGcolor(colors.BlUE)}
+        ></button>
+      </div>
+    );
+  }
+}
+
+export default Color;
