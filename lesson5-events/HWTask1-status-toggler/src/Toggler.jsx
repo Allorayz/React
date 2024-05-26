d326fb853cafb393366d84d001465fb641442e0d
Oleksii Ulianov <alexallorayz@gmail.com>
2024-05-26 13:36:05 +0300

    Status toggler

---
+++ lesson5-events/HWTask1-status-toggler/src/Toggler.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class Toggler extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      defaultToggle: 'Off',
+    };
+  }
+
+  setNewToggle = e => {
+    // const isToggle = () => {
+    //   if (this.state.defaultToggle === 'Off') {
+    //     return (this.state.defaultToggle = 'On');
+    //   } else {
+    //     return (this.state.defaultToggle = 'Off');
+    //   }
+    // };
+
+    const newToggle = this.state.defaultToggle === 'Off' ? 'On' : 'Off';
+    console.log(newToggle);
+    this.setState({
+      // defaultToggle: isToggle(this.state.defaultToggle),
+      defaultToggle: newToggle,
+    });
+  };
+
+  render() {
+    return (
+      <button className="toggler" onClick={() => this.setNewToggle()}>
+        {this.state.defaultToggle}
+      </button>
+    );
+  }
+}
+
+export default Toggler;
