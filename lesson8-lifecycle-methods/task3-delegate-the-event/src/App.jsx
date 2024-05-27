a88e679c206d107b400f54e17c07f7e3b0da8f39
Oleksii Ulianov <alexallorayz@gmail.com>
2024-05-27 20:41:51 +0300

    Подписка на события

---
+++ lesson8-lifecycle-methods/task3-delegate-the-event/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Dimension from './Dimension';
+
+class App extends React.Component {
+  render() {
+    return <Dimension />;
+  }
+}
+
+export default App;
---
+++ lesson8-lifecycle-methods/task3-delegate-the-event/src/Dimension.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Dimension extends React.Component {
+  state = {
+    width: null,
+    height: null,
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+    const { innerWidth, innerHeight } = window;
+    this.setDimensions(innerWidth, innerHeight);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('rezize', this.onResize);
+  }
+
+  onResize = e => {
+    const { innerWidth, innerHeight } = e.target;
+    this.setDimensions(innerWidth, innerHeight);
+  };
+
+  setDimensions = (width, height) => {
+    this.setState({
+      width,
+      height,
+    });
+    document.title = `${width} x ${height}`;
+  };
+
+  render() {
+    const { width, height } = this.state;
+    return <div>{`${width} x ${height}`}</div>;
+  }
+}
+
+export default Dimension;
