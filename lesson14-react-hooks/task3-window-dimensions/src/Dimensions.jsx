4334a44a048efed7a3bc5b7ef5c4cb799c004101
Allorayz <alexallorayz@gmail.com>
2024-06-26 14:17:08 +0300

    Window dimensions - React hooks

--- lesson14-react-hooks/task3-window-dimensions/src/Dimensions.jsx
+++ lesson14-react-hooks/task3-window-dimensions/src/Dimensions.jsx
@@ -1,27 +1,31 @@
 import React, { useState, useEffect } from 'react';
 
 const Dimensions = () => {
-  const [dimensions, setDimension] = useState({
-    width: null,
-    heigth: null,
+  const [dimensions, setDimensions] = useState({
+    width: 0,
+    height: 0,
   });
 
   useEffect(() => {
-    const { innerHeight, innerWidth } = window;
-    setDimension({ width: innerWidth, heigth: innerHeight });
+    const { innerWidth, innerHeight } = window;
+    setDimensions({
+      width: innerWidth,
+      height: innerHeight,
+    });
+
     const handleResize = e => {
-      const { innerHeight, innerWidth } = e.target;
-      setDimension({ width: innerWidth, heigth: innerHeight });
+      const { innerWidth, innerHeight } = e.target;
+      setDimensions({
+        width: innerWidth,
+        height: innerHeight,
+      });
     };
     window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  const { width, heigth } = dimensions;
-  return <div className="dimensions">{`${width}px - ${heigth}px`}</div>;
+  const { width, height } = dimensions;
+  return <div className="dimensions">{`${width}px - ${height}px`}</div>;
 };
 
 export default Dimensions;
